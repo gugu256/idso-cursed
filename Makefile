cde0c079f79752b9029f16c26d5934fc2a9a60a4
gugu256 <gugu256@mail.com>
2023-09-26 17:07:33 +0200

    First Commit

---
+++ Makefile
@@ -0,0 +1,1 @@
+gcc -o idso compiler.c
---
+++ compiler.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+char prog[]; 
+char interpreter_code[] = "int acc = 0; int main() {for (int i = 0; i < sizeof(prog); i++) {char c = prog[i];switch (c) {case 'i':acc++;break;case 'd':acc--;break;case 's':acc = acc * acc;break;case 'o':printf(\"%d \", acc);break;}if (acc >= 256 || acc < 0) {acc = 0;}}return 0;}";
+char file_content[];
+
+int main(int argc, char* argv[]) {
+    strcat(file_content, "#include <stdio.h>\nchar prog[] = \"");
+    strcat(file_content, prog);
+    strcat(file_content, "\";\n");
+    strcat(file_content, interpreter_code);
+    printf("%s", file_content);
+    return 0;
+}
